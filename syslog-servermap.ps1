8321cf6a2cd21a56967ca761258e8cdd0b952283
Sven-Jendrik Timmermann <sven@sjt-consulting.de>
2020-01-07 13:34:29 +0100

    Update syslog-servermap.ps1

--- syslog-servermap.ps1
+++ syslog-servermap.ps1
@@ -1,24 +1,38 @@
+
+
 #Map DC to log server:
+ 
 $servermap = @{
-"DEHAM01-LAB" = "tcp://log.sjt-consulting.int:514";
-"DEHAM01-LAB2" = "tcp://dXXXXt:514";
-"DEHAM01-LAB3" = "tcp://jXXXXX.int:514";
-     
+ 
+    "DC1" = "tcp://syslog01.v-crew.int:514,tcp://syslog02.v-crew.int:514";
+    "DC2" = "tcp://syslog03.v-crew.int:514,tcp://syslog04.v-crew.int:514";
+    "DC3" = "tcp://syslog05.v-crew.int:514,tcp://syslog06.v-crew.int:514";
+ 
 };
-​
+ 
+  
+ 
 foreach ($vmhost in (Get-VMHost)) {
+ 
     $DC = (Get-Datacenter -VMHost $vmhost)
+ 
     echo $vmhost.Name 
+ 
     echo $servermap.($DC.Name)
-    #Assign log server to host:
-    #Set-VMHostSysLogServer -SysLogServer $servermap.($DC.Name) -SysLogServerPort 514 -VMHost $vmhost
-    #$syslog = "`""+$servermap.($DC.Name)+"`""
+ 
+
     $syslog = $servermap.($DC.Name)
-    #Set-VMHostSysLogServer -SysLogServer $syslog -SysLogServerPort 514 -VMHost $vmhost
+ 
     Get-AdvancedSetting -Entity $vmhost -Name "SysLog.Global.loghost" | Set-AdvancedSetting -Value $syslog -Confirm:$false
+ 
     Write-Host "Restarting syslog daemon." -ForegroundColor Green
+ 
     $esxcli = Get-EsxCli -VMHost $vmhost -V2
+ 
     $esxcli.system.syslog.reload.Invoke()
+ 
     Write-Host "Setting firewall to allow Syslog out of $($vmhost)" -ForegroundColor Green
+ 
     Get-VMHostFirewallException -VMHost $vmhost | where {$_.name -eq 'syslog'} | Set-VMHostFirewallException -Enabled:$true
+ 
 }
